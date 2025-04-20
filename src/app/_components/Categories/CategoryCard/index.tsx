9778a4220ba4515fcbf2fd312733e85ea331981d
binary-boon <the.binary.boon@gmail.com>
2025-04-20 21:29:36 +0530

    new data

--- src/app/_components/Categories/CategoryCard/index.tsx
+++ src/app/_components/Categories/CategoryCard/index.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Link from 'next/link'
 
-import { Category } from '../../../../payload/payload-types'
+import { Category, Media } from '../../../../payload/payload-types'
 import { useFilter } from '../../../_providers/Filter'
 
 import classes from './index.module.scss'
@@ -10,6 +10,8 @@ import classes from './index.module.scss'
 type CategoryCardProps = {
   category: Category
 }
+
+
 
 const CategoryCard = ({ category }: CategoryCardProps) => {
   const media = category.media as Media
@@ -19,12 +21,30 @@ const CategoryCard = ({ category }: CategoryCardProps) => {
     <Link
       href="/products"
       className={classes.card}
-      style={{ backgroundImage: `url(${media.url})` }}
+      style={{ backgroundImage: media?.url ? `url(${media.url})` : 'none' }}
       onClick={() => setCategoryFilters([category.id])}
     >
       <p className={classes.title}>{category.title}</p>
     </Link>
   )
 }
+
+
+
+// const CategoryCard = ({ category }: CategoryCardProps) => {
+//   const media = category.media as Media
+//   const { setCategoryFilters } = useFilter()
+
+//   return (
+//     <Link
+//       href="/products"
+//       className={classes.card}
+//       style={{ backgroundImage: `url(${media.url})` }}
+//       onClick={() => setCategoryFilters([category.id])}
+//     >
+//       <p className={classes.title}>{category.title}</p>
+//     </Link>
+//   )
+// }
 
 export default CategoryCard
